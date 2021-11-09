bd0a4f10a6e0eb16cc4233c544a196db35320027
Nicolavickh <nicolavickh@gmail.com>
2021-11-09 15:50:27 +0700

    add comments

--- 010_h8ocbc_converter/010_h8ocbc_converter.py
+++ 010_h8ocbc_converter/010_h8ocbc_converter.py
@@ -1,28 +1,37 @@
 # A function to converto from Celcius to Kelvin or vice versa
 def convertCelciusKelvin(tempterature, command):
     # If statement to see if the funcntion is called to convert Celcius to Kelvin or vice versa
+    # Inputs: 
+    # param 1 temperature: input for temperature | int or float 
+    # param 2 command: input for temparature convert command (to Celcius or Kelvin) | string 
     if(command == "CK"):
-        # Return the converted temperature
+        # Return the converted temperature | int or float
         return tempterature + 273.15
     elif(command == "KC"):
-        # Return the converted temperature
+        # Return the converted temperature | int or float
         return tempterature - 273.15
     return ("Command unrecognized!")
 
-# A function to conver to from Celcius or Kelvin to fahrenheit
+# A function to conver to from Celcius or Kelvin to Fahrenheit
 def convertToFahrenheit(temperature, unit):
     # If statement to see if the funcntion is called to convert to Celcius to Kelvin
+    # Inputs: 
+    # param 1 temperature: input for temperature | int or float 
+    # param 2 unit: input for temparature convert from what unit (to Celcius or Kelvin) | string 
     if (unit == "K"):
-        # Return the converted temperature
+        # Return the converted temperature | int or float
         return (temperature - 273.15) * 9 / 5 + 32
     elif (unit == "C"):
-        # Return the converted temperature
+        # Return the converted temperature | int or float
         return temperature * 9 / 5 + 32
     return "Unit unrecognized!"
 
 #A funtion to convert from Fahrenheit to Celcius and Kelvin
 def convertFromFahrenheit(temperature):
     # Return the converted temperature as a dictionary with 'K' and 'C' key representing Kelvin adn Celcius
+    # Inputs: 
+    # param 1 temperature: input for temperature | int or float 
+    # Return the converted temperature | int or float
     return {'K': (temperature - 32) * 5/9 + 273.15, 'C': (temperature-32) * 5/9}
 
 # Main function to run the program 
